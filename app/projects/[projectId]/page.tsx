5f2d52924e46daaa8cc01777fdcb73678fbe1187
Linn Johansen <linn.es.johansen@gmail.com>
2025-07-18 15:52:58 -0700

    EoD

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -2,13 +2,13 @@ import { projects } from '@/_data/projects';
 import ProjectPage from '@/_components/ui/ProjectPage';
 
 type Props = {
-  params: {
+  params: Promise<{
     projectId: string;
-  };
+  }>;
 };
 
 async function Page({ params }: Props) {
-  const { projectId } = params;
+  const { projectId } = await params;
   const project = projects.find((project) => project.id === projectId);
 
   return <ProjectPage project={project} />;
